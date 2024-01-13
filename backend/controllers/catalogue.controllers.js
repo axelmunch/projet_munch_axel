3cfc551d73c8d1d3fddf2694cdf7fd628e80ad91
Axel Munch <92171197+axelmunch@users.noreply.github.com>
2024-01-13 11:42:09 +0100

    Removed catalogue api default values

--- backend/controllers/catalogue.controllers.js
+++ backend/controllers/catalogue.controllers.js
@@ -1,24 +1,11 @@
-const catalogue = [
-  { ref: "A", name: "Smartphone", price: 899.99 },
-  { ref: "B", name: "Laptop", price: 619.99 },
-  { ref: "C", name: "Headphones", price: 299.99 },
-  { ref: "D", name: "Smartwatch", price: 199.99 },
-  { ref: "E", name: "Robot vacuum cleaner", price: 335.88 },
-  { ref: "F", name: "VR headset", price: 349.99 },
-  { ref: "G", name: "Bicycle", price: 600 },
-  { ref: "H", name: "Printer", price: 120 },
-  { ref: "I", name: "Drone", price: 199.99 },
-  { ref: "J", name: "Shower gel", price: 12.5 },
-];
-
 const db = require("../models");
 const Catalogue = db.catalogue;
 const Op = db.Sequelize.Op;
 
 exports.get = (req, res) => {
-  let searchText = req.query.q ?? "";
+  let searchText = req.query.q; // ?? "";
   searchText = searchText.toLowerCase();
-  let priceInf = req.query.priceInf ?? 0;
+  let priceInf = req.query.priceInf; // ?? 0;
 
   let searchCond = [];
   if (searchText.length > 0) {
@@ -29,7 +16,7 @@ exports.get = (req, res) => {
   }
 
   Catalogue.findAll({
-    limit: 10,
+    limit: 20,
     where: {
       [Op.and]: searchCond,
     },
